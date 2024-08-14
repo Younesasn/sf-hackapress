b6ac9492d2fd8600309f45250e41037a567b4256
ASSANI Younes <younesassani@gmail.com>
2024-08-14 21:51:19 +0200

    update

--- src/Controller/Admin/ItemCrudController.php
+++ src/Controller/Admin/ItemCrudController.php
@@ -7,12 +7,10 @@ use App\Entity\Item;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ItemCrudController extends AbstractCrudController
 {
-    use DisableNewTrait;
+    // use DisableNewTrait;
     public static function getEntityFqcn(): string
     {
         return Item::class;
@@ -22,11 +20,12 @@ class ItemCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            AssociationField::new('command', 'Order'),
+            AssociationField::new('command', 'Order')->hideOnForm(),
             AssociationField::new('product'),
             AssociationField::new('matter'),
             AssociationField::new('status'),
             AssociationField::new('service'),
+            AssociationField::new('employee'),
         ];
     }
 }
