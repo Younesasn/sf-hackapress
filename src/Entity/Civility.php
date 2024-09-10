ab4e3eaedab1fd93c18e5a5068badc7d8e7c2ba4
ASSANI Younes <younesassani@gmail.com>
2024-09-11 01:35:17 +0200

    update CORS Nelmio

--- src/Entity/Civility.php
+++ src/Entity/Civility.php
@@ -7,6 +7,7 @@ use App\Repository\CivilityRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: CivilityRepository::class)]
 #[ApiResource()]
